fd58b0bc443d080a5402581572c0dd6b21b0bb11
piwneuh <filip.pinjuh@gmail.com>
2021-11-01 22:10:44 +0100

    feat: routing

--- patient-application/src/app/app.module.ts
+++ patient-application/src/app/app.module.ts
@@ -2,20 +2,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { RandomNumberGeneratorService } from './feedback.service';
 import { FeedbackComponent } from './feedback/feedback.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FeedbackComponent
+    FeedbackComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule 
+    FormsModule,
+    RouterModule,
+    RouterModule.forRoot([])
   ],
   providers: [RandomNumberGeneratorService],
   bootstrap: [AppComponent]
