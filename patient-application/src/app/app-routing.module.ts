a7e0270fd84e6cac13efaa4c627430bee10254d0
piwneuh <filip.pinjuh@gmail.com>
2021-11-01 23:42:11 +0100

    feat: landing page
    Rough design proposal

--- patient-application/src/app/app-routing.module.ts
+++ patient-application/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule} from '@angular/router';
+import { FeedbackComponent } from './feedback/feedback.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'feedback', component: FeedbackComponent }
 ];
 
 @NgModule({
