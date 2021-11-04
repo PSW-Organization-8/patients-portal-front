f9616a348c6356e876eb0085cc6abdfc44e02942
Mihajlo Mitic RA145/2018 <mihajlomiticmiki@gmail.com>
2021-11-04 17:04:50 +0100

    feat: added router to manager FE

--- manager-application/src/app/app.module.ts
+++ manager-application/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AllFeedbackViewComponent } from './feedbacks/all-feedback-view/all-feedback-view.component';
@@ -11,6 +12,11 @@ import { PharmacyRegistrationComponent } from './pharmacy-registration/pharmacy-
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+
+const routes: Routes = [
+  { path: 'feedbackview', component: AllFeedbackViewComponent },
+  { path: 'pharmacyRegistration', component: PharmacyRegistrationComponent },
+];
 
 @NgModule({
   declarations: [
@@ -25,7 +31,9 @@ import { ToastrModule } from 'ngx-toastr';
     CommonModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
+    RouterModule.forRoot(routes),
   ],
+  exports: [RouterModule],
   providers: [AllFeedbackViewService],
   bootstrap: [AppComponent]
 })
