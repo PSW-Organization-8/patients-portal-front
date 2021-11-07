226f3d8fc55819490fb5ca573daefd87b49976de
vikikukic99 <80200143+vikikukic99@users.noreply.github.com>
2021-11-07 22:20:50 +0100

    feat: display hospital map(all functionalities) (#15)

--- manager-application/src/app/app.module.ts
+++ manager-application/src/app/app.module.ts
@@ -4,22 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { DashboardComponent } from './editor/dashboard.component';
 import { AllFeedbackViewComponent } from './feedbacks/all-feedback-view/all-feedback-view.component';
 import { AllFeedbackViewService } from './feedbacks/all-feedback-view/all-feedback-view.service';
 import { PharmacyRegistrationComponent } from './pharmacy-registration/pharmacy-registration.component';
 
+import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'feedbackview', component: AllFeedbackViewComponent },
   { path: 'pharmacyRegistration', component: PharmacyRegistrationComponent },
 ];
 
 @NgModule({
   declarations: [
+    DashboardComponent,
     AppComponent,
     AllFeedbackViewComponent,
     PharmacyRegistrationComponent,
