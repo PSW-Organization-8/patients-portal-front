b09daad3066e58eb5945645e21fa8ee44b9e4558
piwneuh <filip.pinjuh@gmail.com>
2021-11-18 17:28:40 +0100

    fix: survey validation, added questions

--- patient-application/src/app/survey/survey.component.ts
+++ patient-application/src/app/survey/survey.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { SurveyService } from '../services/survey.service';
 
-declare var step: any;
-
 @Component({
   selector: 'app-survey',
   templateUrl: './survey.component.html',
@@ -15,20 +13,59 @@ export class SurveyComponent implements OnInit {
   constructor(private _surveyService:SurveyService) { }
 
   ngOnInit(): void {
-    new step();
   }
 
   surveys = [
     {id: 1, name:'Tekst 1 pitanja', enum: 0},
-    {id: 2, name:'Tekst 2 pitanjaaaaaaaaaaaaaaaaaaaaaaaaaaa', enum: 0},
-    {id: 3, name:'Tekst 3 pitanja', enum: 1},
-    {id: 4, name:'Tekst 4 pitanjaauauauauauauauauau', enum: 1},
-    {id: 5, name:'Tekst 5 pitanja', enum: 2}
-];
+    {id: 2, name:'Tekst 2 pitanja', enum: 0},
+    {id: 3, name:'Tekst 3 pitanja', enum: 0},
+    {id: 4, name:'Tekst 4 pitanja', enum: 0},
+    {id: 5, name:'Tekst 5pitanja', enum: 0},
+
+    {id: 6, name:'Tekst 6 pitanja', enum: 1},
+    {id: 7, name:'Tekst 7 pitanjaa', enum: 1},
+    {id: 8, name:'Tekst 8 pitanja', enum: 1},
+    {id: 9, name:'Tekst 9 pitanja', enum: 1},
+    {id: 10, name:'Tekst 10 pitanja', enum: 1},
+
+    {id: 11, name:'Tekst 1 pitanja', enum: 2},
+    {id: 12, name:'Tekst 11 pitanja', enum: 2},
+    {id: 13, name:'Tekst 5223 pitanja', enum: 2},
+    {id: 14, name:'Tekst 54 pitanja', enum: 2},
+    {id: 15, name:'Tekst 55 pitanja', enum: 2}
+  ]
+
+sendSurvey(): void {
+    
+  
 
 
-sendSurvey(): void{
-  this._surveyService.sendSurveyToServer(this.surveyRatings);
+  if(this.surveyRatings.length < 15){
+    Swal.fire({
+      icon: 'error',
+      title: 'Error',
+      text: 'Please answer all the questions.',
+    })
+  }
+  else{
+    this._surveyService.sendSurveyToServer(this.surveyRatings);
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'Successfully sent feedback'
+    })
+  }
 }
 
 rateIt(value: number, questionObject: any): void{
