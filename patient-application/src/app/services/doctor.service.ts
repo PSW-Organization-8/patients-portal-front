38546cbe93b42513b62fb5fd0389cecb69386204
RadisaMilovcevic <73460919+RadisaMilovcevic@users.noreply.github.com>
2021-12-10 15:51:19 +0100

    fix: Fixed parameters of get method for appointments by priority

--- patient-application/src/app/services/doctor.service.ts
+++ patient-application/src/app/services/doctor.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { serverPort } from '../app.consts';
 import { Router } from '@angular/router';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Injectable({
     providedIn: 'root',
@@ -18,12 +19,13 @@ import { Router } from '@angular/router';
       return this.http.get<any>(this._url + 'doctor/allDoctors');
     }
 
-    public getDatesByPriority(doctor?: any, firstDate?: any, lastDate?: any, priority?: any) {
-      const params = new HttpParams()
-        .set('firstDate', firstDate)
-        .set('lastDate', lastDate)
-        .set('doctorId', doctor.id)
-        .set('priority', priority);
-      return this.http.get<any>(this._url + 'appointment', {params});
+    public getDatesByPriority(doctor: any, firstDate: any, lastDate: any, priority: any) {
+      let advancedAppointmentDto = {
+        firstDate: firstDate,
+        lastDate: lastDate,
+        doctorId: doctor.id,
+        doctorPriority: priority,
+      };
+      return this.http.get<any>(this._url + 'appointment', {params: advancedAppointmentDto});
     }
   }
