0ce8f3eea66e7495ac31640ef0b634d06d58fdb9
MitarBrankovic <brankovic.ra198.2018@uns.ac.rs>
2021-12-27 23:28:44 +0100

    feat: front logic for login

--- patient-application/src/app/login/login.component.ts
+++ patient-application/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username:string = "";
+  password:string = "";
+  token:any;
 
-  constructor() { }
+  constructor(private _loginService:LoginService) { }
 
   ngOnInit(): void {
   }
 
+
+  login(){
+    if(this.username == "" || this.password == ""){
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Please fill out the text field',
+      })
+    }
+    else{
+      this._loginService.sendLoginFormToServer(this.username, this.password).subscribe(f => this.token = f);
+      alert(this.token)
+      localStorage.setItem('token',JSON.stringify(this.token))
+    }
+  }
 }
---
+++ patient-application/src/app/services/login.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Content } from '@angular/compiler/src/render3/r3_ast';
+import { serverPort } from '../app.consts';
+import { Router } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root',
+  })
+  export class LoginService {
+    private _url = serverPort;
+    constructor(private http: HttpClient, private router:Router) {}
+
+    public getSurveyFromServer(): Observable<any> {
+        return this.http.get<any>(this._url + 'survey');
+    }
+
+    public sendLoginFormToServer(username: any, password: any) {
+        let loginForm = {
+            username: username,
+            password: password,
+          };
+
+          let params = new HttpParams()
+            .append('username', username)
+            .append('password', password)
+          return this.http.post<any>(this._url + 'login', loginForm);
+    }
+}
